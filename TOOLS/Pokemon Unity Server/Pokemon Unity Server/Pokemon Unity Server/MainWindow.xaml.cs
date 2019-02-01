445a97c796769600bec537a9e3daf0b5e472b23d
Velorexe <david.bruschke@gmail.com>
2019-02-01 19:48:56 +0100

    Updated MainWindow.xaml.cs

--- TOOLS/Pokemon Unity Server/Pokemon Unity Server/Pokemon Unity Server/MainWindow.xaml.cs
+++ TOOLS/Pokemon Unity Server/Pokemon Unity Server/Pokemon Unity Server/MainWindow.xaml.cs
@@ -1,27 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Net;
-using System.Net.Sockets;
-using System.Threading;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.Runtime.Serialization;
-using System.IO;
-using PokemonUnity.Networking.Packets;
-using PokemonUnity.Networking.Packets.Incoming;
-using PokemonUnity.Networking.Packets.Outgoing;
-using System.Reflection;
 using Pokemon_Unity_Server.Networking;
 
 namespace Pokemon_Unity_Server
