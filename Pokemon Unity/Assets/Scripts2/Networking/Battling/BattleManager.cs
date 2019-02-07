bb7079dd58c417b6f92b8004dd947e649ebd588c
Velorexe <david.bruschke@gmail.com>
2019-02-07 18:27:28 +0100

    Created new BattleManager class

---
+++ Pokemon Unity/Assets/Scripts2/Networking/Battling/BattleManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokemonUnity.Networking.Battling
+{
+    public static class BattleManager
+    {
+        private static int battleRoomId;
+
+        public static void InitiateBattle()
+        {
+
+        }
+    }
+}
